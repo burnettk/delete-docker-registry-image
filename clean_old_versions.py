887c497572bf462872145f0e2d86a85e67992e52
Anthony Comtois <anthony.comtois@sky.uk>
2017-01-18 14:16:40 +0000

    fix run when non tags exists + add Headers

--- clean_old_versions.py
+++ clean_old_versions.py
@@ -20,14 +20,17 @@ def valid_date(date_str):
         raise argparse.ArgumentTypeError(msg)
 
 def get_created_date_for_tag(tag, repository, auth, args):
+    headers = {'Accept': 'application/vnd.docker.distribution.manifest.v2+json'}
+
     response = requests.get(args.registry_url + "/v2/" + repository + "/manifests/" + tag,
-                            auth=auth, verify=args.no_check_certificate)
+                            auth=auth, verify=args.no_check_certificate, headers=headers)
+
     if response.json()['schemaVersion'] == 1:
         created_str = json.loads(response.json()['history'][0]['v1Compatibility'])['created'].split(".")[0]
     elif response.json()['schemaVersion'] == 2:
         digest = response.json()["config"]["digest"]
         response = requests.get(args.registry_url + "/v2/" + repository + "/blobs/" + digest,
-                                auth=auth, verify=args.no_check_certificate)
+                                auth=auth, verify=args.no_check_certificate, headers=headers)
         created_str = response.json()['created'].split(".")[0]
     return(datetime.strptime(created_str,DATE_FORMAT))
 
@@ -111,50 +114,53 @@ def main():
 
             # For each tag, check it does not matches with args.exclude
             matching_tags = []
-            for tag in tags:
-                if not args.exclude or not re.search(args.exclude, tag):
-                    if not args.include or re.search(args.include, tag):
-                        matching_tags.append(tag)
+            if tags is not None:
+                for tag in tags:
+                    if not args.exclude or not re.search(args.exclude, tag):
+                        if not args.include or re.search(args.include, tag):
+                            matching_tags.append(tag)
 
-            # Sort tags
-            if args.order == 'name':
-                order_fn = lambda s: LooseVersion(re.sub('[^0-9.]', '9', s))
+                # Sort tags
+                if args.order == 'name':
+                    order_fn = lambda s: LooseVersion(re.sub('[^0-9.]', '9', s))
+                else:
+                    order_fn = lambda s: get_created_date_for_tag(s, repository, auth, args)
+
+                matching_tags.sort(key=order_fn)
+
+                # Set number of last tags to keep to the default value of 5
+                # if 'last' is not set
+                if args.last is None:
+                    args.last = 5
+
+                # Delete all except N last items
+                if args.last is not None and args.last > 0:
+                    matching_tags = matching_tags[:-args.last]
+                else:
+                    matching_tags = matching_tags
+
+                tags_to_delete = []
+                if args.before or args.after:
+                    for tag in matching_tags:
+                        created = get_created_date_for_tag(tag, repository, auth, args)
+
+                        if (not args.before or created < args.before) and (not args.after or created > args.after) :
+                            tags_to_delete.append(tag)
+                else:
+                    tags_to_delete = matching_tags
+
+                for tag in tags_to_delete:
+                    command2run = "{0} --image {1}:{2}". \
+                        format(args.script_path, repository, tag)
+                    if args.dry_run :
+                        print("Simulate deletion of {0}:{1}".format(repository, tag))
+                        command2run += " --dry-run"
+                    print("Running: {0}".format(command2run))
+                    out = subprocess.Popen(command2run, shell=True, stdout=subprocess.PIPE,
+                                           stderr=subprocess.STDOUT).stdout.read()
+                    print(out)
             else:
-                order_fn = lambda s: get_created_date_for_tag(s, repository, auth, args)
-
-            matching_tags.sort(key=order_fn)
-
-            # Set number of last tags to keep to the default value of 5
-            # if 'last' is not set
-            if args.last is None:
-                args.last = 5
-
-            # Delete all except N last items
-            if args.last is not None and args.last > 0:
-                matching_tags = matching_tags[:-args.last]
-            else:
-                matching_tags = matching_tags
-
-            tags_to_delete = []
-            if args.before or args.after:
-                for tag in matching_tags:
-                    created = get_created_date_for_tag(tag, repository, auth, args)
-
-                    if (not args.before or created < args.before) and (not args.after or created > args.after) :
-                        tags_to_delete.append(tag)
-            else:
-                tags_to_delete = matching_tags
-
-            for tag in tags_to_delete:
-                command2run = "{0} --image {1}:{2}". \
-                    format(args.script_path, repository, tag)
-                if args.dry_run :
-                    print("Simulate deletion of {0}:{1}".format(repository, tag))
-                    command2run += " --dry-run"
-                print("Running: {0}".format(command2run))
-                out = subprocess.Popen(command2run, shell=True, stdout=subprocess.PIPE,
-                                       stderr=subprocess.STDOUT).stdout.read()
-                print(out)
+                print("No tags availables for " + repository)
 
 
 if __name__ == '__main__':
